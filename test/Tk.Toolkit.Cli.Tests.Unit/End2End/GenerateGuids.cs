e45a653797df5d4471e5794957480b8ce5f9cafa
tonycknight <tonycknight@users.noreply.github.com>
2022-09-03 18:24:16 +0100

    very basic e2e tests

---
+++ test/Tk.Toolkit.Cli.Tests.Unit/End2End/GenerateGuids.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Tk.Toolkit.Cli.Tests.Unit.End2End
+{
+    public class GenerateGuids
+    {
+        [Fact]
+        public void GenerateGuids_ReturnsOk()
+        {
+            var rc = Program.Main(new[] { "guid" });
+
+            rc.Should().Be(0);
+        }
+
+        [Fact]
+        public void GenerateGuids_AppliedGenerations_ReturnsOk()
+        {
+            var rc = Program.Main(new[] { "guid", "-g 10" });
+
+            rc.Should().Be(0);
+        }
+                
+        [Fact]
+        public void GenerateGuids_InvalidGenerations_ReturnsFailure()
+        {
+            var rc = Program.Main(new[] { "guid", "-g a" });
+
+            rc.Should().Be(1);
+        }
+
+        
+    }
+}
