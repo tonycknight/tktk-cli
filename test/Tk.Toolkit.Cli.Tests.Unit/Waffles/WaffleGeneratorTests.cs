72bc15f957844346e8789f54874cb8c769a2e47e
tonycknight <tonycknight@users.noreply.github.com>
2022-10-22 15:09:20 +0100

    add very basic, gen covering tests

---
+++ test/Tk.Toolkit.Cli.Tests.Unit/Waffles/WaffleGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FsCheck;
+using FsCheck.Xunit;
+using NSubstitute;
+using Tk.Toolkit.Cli.Waffle;
+
+namespace Tk.Toolkit.Cli.Tests.Unit.Waffles
+{
+    public class WaffleGeneratorTests
+    {
+        [Property(Verbose = true, MaxTest = 1000)]
+        public bool Generate_GeneratesText(PositiveInt paragraphs, bool title)
+        {
+            var rng = Substitute.For<IRng>();
+            
+            var gen = new WaffleGenerator(rng);
+
+            var result = gen.Generate(paragraphs.Get, title);
+
+            return result.ToString().Trim().Length > 0;
+        }
+    }
+}
