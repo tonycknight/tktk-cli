4388d3e3e488e146a79aba3d453e50c47b534845
tonycknight <tonycknight@users.noreply.github.com>
2022-11-19 18:52:03 +0000

    remove param

--- src/Tk.Toolkit.Cli/Commands/UsernameGeneratorCommand.cs
+++ src/Tk.Toolkit.Cli/Commands/UsernameGeneratorCommand.cs
@@ -21,14 +21,10 @@ namespace Tk.Toolkit.Cli.Commands
         [Option(CommandOptionType.SingleValue, Description = "The number of usernames to generate.", LongName = "gen", ShortName = "g")]
         public int Generations { get; set; } = DefaultUsernameCount;
 
-        [Option(CommandOptionType.SingleValue, Description = "The character length of each generated username.", LongName = "len", ShortName = "l")]
-        public int UnLength { get; set; } = DefaultUsernameLength;
-
         public int OnExecute()
         {
             var generations = Generations.ApplyDefault(x => x < 1, DefaultUsernameCount);
-            var pwLen = UnLength.ApplyDefault(x => x < 1, DefaultUsernameLength);
-
+            
             var pws = Enumerable.Range(0, generations)
                                 .Select(_ => _unGenerator.Generate())
                                 .ToSpectreList();
