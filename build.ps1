e03e0783f6265f838c15601277078bad19f982d6
Tony Knight <tonycknight@users.noreply.github.com>
2025-09-23 21:37:03 +0100

    cleanup

--- build.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Mandatory=$false)]
-    [string]$Target = "All"
-)
-$ErrorActionPreference = 'Stop'
-
-& dotnet tool restore
-
-& dotnet run --project .\build\build.fsproj -c Release -t $Target
