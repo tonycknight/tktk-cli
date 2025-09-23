31fe5d94382fe449fb19b602a222065b02cf87b7
Tony Knight <tonycknight@users.noreply.github.com>
2025-09-23 21:34:51 +0100

    Generate passwords with explicitly character types

---
+++ test/Tk.Toolkit.Cli.Tests.Unit/Passwords/Arbitraries.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using FsCheck;
+using FsCheck.Fluent;
+
+namespace Tk.Toolkit.Cli.Tests.Unit.Passwords
+{
+    internal static class PasswordLengths
+    {
+        public static Arbitrary<int> GetGenerator() =>
+            ArbMap.Default.ArbFor<int>().Generator
+                .Where(x => x > 4)
+                .ToArbitrary();
+    }
+}
