e55a7e76e483ba0b8d55be049b92d5c1a65f5918
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-01 02:46:45 +0000

    json formatting shell

---
+++ src/Tk.Toolkit.Cli/Commands/FormatJsonCommand.cs
@@ -0,0 +1,35 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Spectre.Console;
+
+namespace Tk.Toolkit.Cli.Commands
+{
+    [Command("json", Description = "Json formatting")]
+    internal class FormatJsonCommand
+    {
+        private readonly IAnsiConsole _console;
+
+        public FormatJsonCommand(IAnsiConsole console)
+        {
+            _console = console;
+        }
+
+        [Argument(0, Description = "The Json to convert.", Name = "value")]
+        public string? Value { get; set; }
+
+        // TODO: options? colouring? indentation?
+
+        public int OnExecute()
+        {
+            try
+            {
+                // TODO: 
+                return true.ToReturnCode();
+            }
+            catch (Exception ex)
+            {
+                _console.Write(new Markup($"[red]Invalid value.[/]"));
+                return false.ToReturnCode();
+            }
+        }
+    }
+}
---
+++ src/Tk.Toolkit.Cli/JsonFormatting/JsonFormatter.cs
@@ -0,0 +1,16 @@
+﻿namespace Tk.Toolkit.Cli.JsonFormatting
+{
+    internal interface IJsonFormatter
+    {
+        string Format(string json);
+    }
+
+    internal class JsonFormatter : IJsonFormatter
+    {
+        public string Format(string json)
+        {
+            // TODO: 
+            return json;
+        }
+    }
+}
