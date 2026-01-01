d101d1f0bb838214613e7c8f5f8f2570ea5f78e0
Tony Knight <tonycknight@users.noreply.github.com>
2026-01-01 02:59:19 +0000

    basic json formatting

--- src/Tk.Toolkit.Cli/Commands/FormatJsonCommand.cs
+++ src/Tk.Toolkit.Cli/Commands/FormatJsonCommand.cs
@@ -1,5 +1,6 @@
 ﻿using McMaster.Extensions.CommandLineUtils;
 using Spectre.Console;
+using Tk.Toolkit.Cli.JsonFormatting;
 
 namespace Tk.Toolkit.Cli.Commands
 {
@@ -7,29 +8,49 @@ namespace Tk.Toolkit.Cli.Commands
     internal class FormatJsonCommand
     {
         private readonly IAnsiConsole _console;
+        private readonly IJsonFormatter _jsonFormatter;
 
-        public FormatJsonCommand(IAnsiConsole console)
+        public FormatJsonCommand(IAnsiConsole console, IJsonFormatter jsonFormatter)
         {
             _console = console;
+            _jsonFormatter = jsonFormatter;
         }
 
         [Argument(0, Description = "The Json to convert.", Name = "value")]
-        public string? Value { get; set; }
+        public string Value { get; set; } = "";
 
-        // TODO: options? colouring? indentation?
+        [Option(CommandOptionType.SingleValue, Description = "Indent the json true/false.", LongName = "indent", ShortName = "i")]
+        public bool Indent { get; set; } = true;
+
+        [Option(CommandOptionType.SingleValue, Description = "Colourise the json true/false.", LongName = "colour", ShortName = "c")]
+        public bool Colourise { get; set; } = true;
 
         public int OnExecute()
         {
             try
             {
-                // TODO: 
+                var result = _jsonFormatter.Format(Value, Indent, Colourise);
+
+                _console.Write(result);
+
                 return true.ToReturnCode();
             }
-            catch (Exception ex)
+            catch (Newtonsoft.Json.JsonReaderException ex)
             {
                 _console.Write(new Markup($"[red]Invalid value.[/]"));
                 return false.ToReturnCode();
             }
+            catch (Newtonsoft.Json.JsonException ex)
+            {
+                _console.Write(new Markup($"[red]Invalid value.[/]"));
+                return false.ToReturnCode();
+            }
+            catch (Exception ex)
+            {
+                _console.Write(new Markup($"[red]{ex.Message}[/]"));
+                return false.ToReturnCode();
+            }
+
         }
     }
 }
--- src/Tk.Toolkit.Cli/JsonFormatting/JsonFormatter.cs
+++ src/Tk.Toolkit.Cli/JsonFormatting/JsonFormatter.cs
@@ -1,16 +1,34 @@
-﻿namespace Tk.Toolkit.Cli.JsonFormatting
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Tk.Toolkit.Cli.JsonFormatting
 {
     internal interface IJsonFormatter
     {
-        string Format(string json);
+        string Format(string json, bool indent, bool colourise);
     }
 
     internal class JsonFormatter : IJsonFormatter
     {
-        public string Format(string json)
+        public string Format(string json, bool indent, bool colourise)
         {
-            // TODO: 
-            return json;
+            var settings = CreateSettings(indent, colourise);
+
+            var x = JsonConvert.DeserializeObject(json, settings);
+
+            return JsonConvert.SerializeObject(x, settings); 
+        }
+
+        private JsonSerializerSettings CreateSettings(bool indent, bool colourise)
+        {
+            return new JsonSerializerSettings
+            {
+                Formatting = indent ? Formatting.Indented : Formatting.None,
+                Converters = new List<JsonConverter>
+                {
+                    new StringEnumConverter()
+                }
+            };
         }
     }
 }
