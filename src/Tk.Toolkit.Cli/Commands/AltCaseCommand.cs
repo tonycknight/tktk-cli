3b816cf88c475edb089f104fcb1efa6224c9f169
Tony Knight <tonycknight@users.noreply.github.com>
2026-09-05 12:58:11 +0100

    clean

--- src/Tk.Toolkit.Cli/Commands/AltCaseCommand.cs
+++ src/Tk.Toolkit.Cli/Commands/AltCaseCommand.cs
@@ -6,11 +6,10 @@ namespace Tk.Toolkit.Cli.Commands;
 
 [Command("altcase", Description = "Alternating case")]
 internal class AltCaseCommand(IAnsiConsole console, AltCaseTextFormatter formatter)
-{    
-    // TODO: needs to ignore quotes
-    [Argument(0, Description = "The text to convert.", Name = "value")]
+{
+    [Argument(0, Description = "The text to convert.", Name = "value")]    
     public string Value { get; set; } = "";
-
+    
     public int OnExecute()
     {        
         var result = formatter.Format(Value);
